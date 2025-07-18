6dbf56b06455aecf04eabdbe1a7ae7a9b121509c
mia-oww <miafendi01@gmail.com>
2025-07-17 21:50:46 -0500

    added .env and fixed key

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+#from django.core.management.utils import get_random_secret_key
+#print(get_random_secret_key())
+from decouple import config
+
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-iwjy_i_t5dwx6j4obv9*!*stzek5zxmxo0ve56o&vzs!_wnh(4'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
